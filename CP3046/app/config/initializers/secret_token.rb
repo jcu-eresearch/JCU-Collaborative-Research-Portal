92fccb7197eeb97443ce393b8def90edbf7a6a0a
Robert Pyke <robert.j.pyke@gmail.com>
2011-03-02 12:05:31 +0000

    Added a rails project to the repo
    git-svn-id: svn+ssh://brimstone4814.dyndns.org/var/svn/repos@3 b1a53ba2-5c11-429d-a996-d955b03486fd

---
+++ CP3046/app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '22bf51b1d4abbe0a02175a47ce71bc51ca2c31770434fd763fd35ea5d34382af1534a169261d750ef66bb59b55279fdfc4e980bbb6ca00245eb84d8bfc81513f'
