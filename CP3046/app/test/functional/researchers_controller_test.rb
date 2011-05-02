c377c1236c63a4212c4a36f5098959d77de0be1e
Robert Pyke <robert.j.pyke@gmail.com>
2011-05-02 07:26:37 +0000

    Added more tests for rss feed authentication
    git-svn-id: svn+ssh://brimstone4814.dyndns.org/var/svn/repos@68 b1a53ba2-5c11-429d-a996-d955b03486fd

--- CP3046/app/test/functional/researchers_controller_test.rb
+++ CP3046/app/test/functional/researchers_controller_test.rb
@@ -16,6 +16,29 @@ class ResearchersControllerTest < ActionController::TestCase
     get :show, { :id => @researcher.to_param }, { :jc_number => @researcher.to_param }
     assert_response :success
   end
+  
+  test "shouldn't show posts (rss) without http_auth provided" do
+    get :posts, { :format => 'rss', :id => @researcher.to_param }, { :jc_number => @researcher.to_param }
+    assert_response :unauthorized
+  end
+
+  test "should show posts (rss) with http_auth provided"  do
+    @request.env['HTTP_AUTHORIZATION'] = encode_credentials(@researcher.jc_number, 'one')
+    get :posts, { :format => 'rss', :id => @researcher.to_param }
+    assert_response :success
+  end
+  
+  test "should show posts (rss) with other researchers http_auth provided"  do
+    @request.env['HTTP_AUTHORIZATION'] = encode_credentials(@researcher.jc_number, 'bar')
+    get :posts, { :format => 'rss', :id => @other_researcher.to_param }
+    assert_response :success
+  end
+  
+  test "shouldn't show posts (rss) with bad http_auth provided"  do
+    @request.env['HTTP_AUTHORIZATION'] = encode_credentials('foo', 'bar')
+    get :posts, { :format => 'rss', :id => @other_researcher.to_param }
+    assert_response :unauthorized
+  end
 
   test "should get researcher's account" do
     get :account, { :id => @researcher.to_param }, { :jc_number => @researcher.to_param }
@@ -25,6 +48,23 @@ class ResearchersControllerTest < ActionController::TestCase
   test "should not get other researcher's account" do
     get :account, { :id => @other_researcher.to_param }, { :jc_number => @researcher.to_param }
 	assert_redirected_to researcher_path(@other_researcher)
+  end
+  
+  test "shouldn't show account (rss) without http_auth provided" do
+    get :account, { :format => 'rss', :id => @researcher.to_param }, { :jc_number => @researcher.to_param }
+    assert_response :unauthorized
+  end
+
+  test "should show account (rss) with http_auth provided"  do
+    @request.env['HTTP_AUTHORIZATION'] = encode_credentials(@researcher.jc_number, 'one')
+    get :account, { :format => 'rss', :id => @researcher.to_param }
+    assert_response :success
+  end
+  
+  test "shouldn't show account (rss) with other researchers http_auth provided"  do
+    @request.env['HTTP_AUTHORIZATION'] = encode_credentials(@researcher.jc_number, 'bar')
+    get :account, { :format => 'rss', :id => @other_researcher.to_param }
+    assert_response :unauthorized
   end
 
   test "should get edit" do
